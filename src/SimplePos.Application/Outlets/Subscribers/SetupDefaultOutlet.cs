8f92de84cce332e422416543d387fd8a00b6d91f
HikingFish <cheongwaikian1@gmail.com>
2026-08-04 01:14:46 +0800

    feat: enhance SetupDefaultOutlet to handle outlet creation errors

--- src/SimplePos.Application/Outlets/Subscribers/SetupDefaultOutlet.cs
+++ src/SimplePos.Application/Outlets/Subscribers/SetupDefaultOutlet.cs
@@ -8,9 +8,28 @@ namespace SimplePos.Application.Outlets.Subscribers;
 
 public class SetupDefaultOutlet : IDomainEventHandler<CompanyCreatedDomainEvent>
 {
+    private readonly IOutletRepository _outletRepository;
+
+    public SetupDefaultOutlet(IOutletRepository outletRepository)
+    {
+        _outletRepository = outletRepository;
+    }
+
     public Task Handle(CompanyCreatedDomainEvent @event, CancellationToken cancellationToken = default)
     {
         Result<Outlet> resultOutlet = Outlet.Create(@event.CompanyId, @event.Name, @event.Address, @event.PhoneNumber);
+
+        if (resultOutlet.IsFailure)
+        {
+            throw new InvalidOperationException($"Failed to create default outlet for company with ID: {@event.CompanyId}. Error: {resultOutlet.Error}");
+        }
+
+        if (resultOutlet.Data is null)
+        {
+            throw new InvalidOperationException($"Failed to create default outlet for company with ID: {@event.CompanyId}. Outlet data is null.");
+        }
+
+        _outletRepository.AddOutletAsync(resultOutlet.Data);
         Debug.WriteLine($"Default outlet created for company with ID: {@event.CompanyId}");
         return Task.CompletedTask;
     }
